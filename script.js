8aab1e5ae856ef0fadcd4b4e7d701ace0269d592
Herman <smurovnemez@mail.ru>
2022-07-09 17:57:30 +0300

    Early bad setting game

--- script.js
+++ script.js
@@ -1,64 +1,102 @@
-const game = () => {
-    let lvl = 3
-    let HP = 3
-    let damage = 1
-    let lvlCounter = 1
+const renderLvl = () => {
+    const postValueBtn = document.querySelector('.postValueBtn')
+    const input = document.querySelector('.input')
+    const startRenderGame = document.querySelector('.start')
+    const endRenderGame = document.querySelector('.end')
+    const wrapperRenderGame = document.querySelector('.hidden')
+    let getAnswer
+    let randomValue
 
-    while (HP > 0) {
-        let randomValue = Math.floor(Math.random() * lvl)
+    let div = document.createElement('div')
+    let divFalse = document.createElement('div')
+    let divTrue = document.createElement('div')
+
+    startRenderGame.addEventListener('click', () => {
+        settingGame = {
+            lvl: 3,
+            lvlCounter: 1,
+            HP: 3,
+            min: 1
+        }
+        renderLvl()
+    })
+
+    endRenderGame.addEventListener('click', () => {
+        div.remove()
+    })
+
+    postValueBtn.addEventListener('click', () => {
+        getAnswer = +input.value
+        gameScript()
+        console.log(getAnswer)
+        renderLvl()
+    })
+
+    let settingGame = {
+        lvl: 3,
+        lvlCounter: 1,
+        HP: 3,
+        min: 1
+    }
+
+    let getValue = { getLvl: [] }
+
+    const renderLvl = () => {
+
+        div.innerHTML = `
+        <div>  Lvl ${settingGame.lvlCounter} угадайте от 1 до ${settingGame.lvl} ваши жизни ${'🧡'.repeat(settingGame.HP)} </div `
+        wrapperRenderGame.append(div)
+
+    }
+
+    const renderLvlTrue = () => {
+        divTrue.innerHTML = `
+        <div> Вы прошли ${settingGame.lvlCounter}Lvl. Загаданное число ${settingGame.randomValue}, ваше ${settingGame.answer} </div `
+            // if (answer === randomValue) {
+            //     wrapperRenderGame.append(divTrue)
+            // }
+    }
+
+
+    const renderLvlFalse = () => {
+        divFalse.innerHTML = `
+            <div> Вы не прошли ${settingGame.lvlCounter}Lvl. Загаданное число ${settingGame.randomValue}, ваше ${settingGame.answer} </div `
+            // if (answer !== randomValue) {
+            //     div.remove()
+            //     wrapperRenderGame.append(divFalse)
+            // }
+    }
+
+
+    const gameScript = () => {
+        randomValue = Math.floor(Math.random() * (settingGame.min) * settingGame.lvl)
+            // if (settingGame.HP > 0) {
         console.log(randomValue)
-        let answer = +input
-        console.log(answer)
-
-
-
-        if (randomValue !== answer) {
-            HP = HP - damage
+            // }
+        if (randomValue !== getAnswer) {
+            settingGame.HP = settingGame.HP - settingGame.damage
+            alert(`Загаданное число ${randomValue}, ваше ${getAnswer}`)
         } else {
-            alert('Ygadal')
-            lvlCounter++
-            lvl++
-            HP = 3
+            renderLvlTrue()
+            settingGame.lvlCounter++
+                settingGame.lvl++
+                settingGame.HP = 3
+            alert(`Вы угадали загаданное число ${randomValue}, ваше ${getAnswer} `)
         }
 
-        if (HP === 0) {
+        if (settingGame.HP === 0) {
             alert('Закончились жизни')
 
         }
 
-        if (lvlCounter === 6) {
+        if (settingGame.lvlCounter === 6) {
             alert('Прошел игру')
             return true
         }
+
     }
+    gameScript()
+
+
 }
-
-const input = document.querySelector('.input').value;
-
-const startRenderGame = () => {
-    const btnStart = document.querySelector('.start')
-    const btnEnd = document.querySelector('.end')
-    let obj = {
-        lvl: 3,
-        HP: 3,
-        lvlCounter: 1
-    }
-    const wrapperGame = document.querySelector('.wrapper__game')
-
-    let div = document.createElement('div')
-
-    div.innerHTML = ` 
-            <p>Lvl ${obj.lvlCounter}  уагадайте число от 1 до ${obj.lvl} ${'❤'.repeat(obj.HP)}</p>
-
-                    `
-
-    btnStart.addEventListener('click', () => {
-        wrapperGame.append(div)
-        game()
-    })
-
-    btnEnd.addEventListener('click', () => {
-        div.remove()
-    })
-}
-startRenderGame()
+renderLvl()
